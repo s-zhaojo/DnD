3cca5ab515d52098903656bc043011a9e5422c43
s-zhaojo <56804773+s-zhaojo@users.noreply.github.com>
2025-03-15 13:14:44 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to DnD the website</h1>
+        <h1>Welcome to DnD the website hello</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload. Test
         </p>
