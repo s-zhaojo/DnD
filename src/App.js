d957f513b18955ce50b9dfe6d9f8ac464a731c0e
s-zhaojo <56804773+s-zhaojo@users.noreply.github.com>
2025-03-15 13:48:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to the Random Coupon website</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Generate Coupon
         </p>
         <a
           className="App-link"
