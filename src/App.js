e18fa85529516eba4a7327a2e7ecfd34bb2ff574
s-zhaojo <56804773+s-zhaojo@users.noreply.github.com>
2025-03-15 12:59:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to My React App</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Test
         </p>
         <a
           className="App-link"
