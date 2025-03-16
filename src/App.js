c8e1b3d8167d59860bf8e8076f386343e2ae9a40
s-zhaojo <56804773+s-zhaojo@users.noreply.github.com>
2025-03-16 11:31:51 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { useState } from 'react';
-import Spline from '@splinetool/react-spline/next';
 
 function App() {
   const happyActions = [
@@ -50,34 +49,26 @@ function App() {
   };
 
   return (
-    <div className="app-container">
-      <div className="spline-container">
-        <Spline
-          scene="https://prod.spline.design/0aj50JTrcxxmzvst/scene.splinecode"
-        />
+    <div className="container">
+      <div className="dice-section" id="happy">
+        <h2>Roll the Happy D50</h2>
+        <button onClick={() => rollDice("happy")}>Roll</button>
+        {happyRoll && <p>You rolled: {happyRoll}</p>}
+        {happyAction && <p>Action: {happyAction}</p>}
       </div>
 
-      <div className="dice-container">
-        <div className="dice-section" id="happy">
-          <h2>Roll the Happy D50</h2>
-          <button onClick={() => rollDice("happy")}>Roll</button>
-          {happyRoll && <p>You rolled: {happyRoll}</p>}
-          {happyAction && <p>Action: {happyAction}</p>}
-        </div>
+      <div className="dice-section" id="mad">
+        <h2>Roll the Mad D50</h2>
+        <button onClick={() => rollDice("mad")}>Roll</button>
+        {madRoll && <p>You rolled: {madRoll}</p>}
+        {madAction && <p>Action: {madAction}</p>}
+      </div>
 
-        <div className="dice-section" id="mad">
-          <h2>Roll the Mad D50</h2>
-          <button onClick={() => rollDice("mad")}>Roll</button>
-          {madRoll && <p>You rolled: {madRoll}</p>}
-          {madAction && <p>Action: {madAction}</p>}
-        </div>
-
-        <div className="dice-section" id="sad">
-          <h2>Roll the Sad D50</h2>
-          <button onClick={() => rollDice("sad")}>Roll</button>
-          {sadRoll && <p>You rolled: {sadRoll}</p>}
-          {sadAction && <p>Action: {sadAction}</p>}
-        </div>
+      <div className="dice-section" id="sad">
+        <h2>Roll the Sad D50</h2>
+        <button onClick={() => rollDice("sad")}>Roll</button>
+        {sadRoll && <p>You rolled: {sadRoll}</p>}
+        {sadAction && <p>Action: {sadAction}</p>}
       </div>
     </div>
   );
